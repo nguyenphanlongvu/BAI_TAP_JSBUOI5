a1e9a275efb2c14423260a215cea4c218be45f01
nguyenphanlongvu <vuvt6019@gmail.com>
2023-08-23 19:41:44 +0700

    update file bai tap

--- index.js
+++ index.js
@@ -50,3 +50,76 @@ function tinhTien() {
 
 
 }
+// bài 3
+function calculateTax() {
+    var fullName = document.getElementById("fullName").value;
+    var totalIncome = parseFloat(document.getElementById("totalIncome").value);
+    var dependents = parseInt(document.getElementById("dependents").value);
+
+    if (!fullName || isNaN(totalIncome) || isNaN(dependents)) {
+        document.getElementById("taxResult").textContent = "Vui lòng nhập đầy đủ thông tin";
+        return;
+    }
+
+   var taxableIncome = totalIncome  - 4000000 - dependents * 1600000;
+ var taxRate = calculateTaxRate(taxableIncome);
+    var taxAmount = taxableIncome * taxRate;
+
+    document.getElementById("taxResult").textContent = `${fullName}, Thuế thu nhập cá nhân phải trả: ${taxAmount} VND`;
+}
+
+function calculateTaxRate(taxableIncome) {
+    if (taxableIncome <= 60000000) {
+        return 0.05;
+    } else if (taxableIncome <= 120000000) {
+        return 0.1;
+    } else if (taxableIncome <= 210000000) {
+        return 0.15;
+    } else if (taxableIncome <= 384000000) {
+        return 0.2;
+    } else if (taxableIncome <= 624000000) {
+        return 0.25;
+    } else if (taxableIncome <= 960000000) {
+        return 0.3;
+    } else {
+        return 0.35;
+    }
+}
+//bài 4
+function toggleKetNoi() {
+    var loaiKhachHang = document.getElementById("loaiKhachHang").value;
+    var soKetNoiInput = document.getElementById("soKetNoi");
+
+    if (loaiKhachHang === "nhaDan") {
+        soKetNoiInput.disabled = true;
+        soKetNoiInput.value = "";
+    } evar
+        soKetNoiInput.disabled = false;
+    }
+
+function tinhHoaDon() {
+    var maKhachHang = document.getElementById("maKhachHang").value;
+    var loaiKhachHang = document.getElementById("loaiKhachHang").value;
+    var soKetNoi = parseInt(document.getElementById("soKetNoi").value);
+    var soKenhCaoCap = parseInt(document.getElementById("soKenhCaoCap").value);
+
+    let phiXuLy = 0;
+    let phiDichVuCoBan = 0;
+    let phiKenhCaoCap = 0;
+
+    if (loaiKhachHang === "nhaDan") {
+        phiXuLy = 4.5;
+        phiDichVuCoBan = 20.5;
+        phiKenhCaoCap = 7.5 * soKenhCaoCap;
+    } else if (loaiKhachHang === "doanhNghiep") {
+        phiXuLy = 15;
+        phiDichVuCoBan = 75 + (soKetNoi - 10) * 5;
+        phiKenhCaoCap = 50 * soKenhCaoCap;
+    }
+
+    var tongHoaDon = phiXuLy + phiDichVuCoBan + phiKenhCaoCap;
+
+    document.getElementById("hoaDonResult").textContent = `Mã khách hàng: ${maKhachHang}\nTổng hóa đơn: ${tongHoaDon} $`;
+}
+
+
